9c2ecb528b374eeb3453947dc91e53337b57958d
Murmurline <steward379@gmail.com>
2023-06-30 11:48:09 +0800

    "flask try"

---
+++ index.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+# 函式的裝飾器 (Decorator)：函釋為基礎提供的附加功能
+@app.route("/") 
+def home():
+    return "Hello Flask"
+
+@app.route("/test)
+def test():
+    return "test"
+    
+if __name__ == "__main__":
+    app.run() 
