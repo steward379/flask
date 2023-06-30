7606525319a4274c60097a1591a5630433984e62
Murmurline <steward379@gmail.com>
2023-06-30 12:12:56 +0800

    "flask vercel"

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 # 函式的裝飾器 (Decorator)：函釋為基礎提供的附加功能
@@ -6,9 +7,9 @@ app = Flask(__name__)
 def home():
     return "Hello Flask"
 
-@app.route("/test)
+@app.route("/test")
 def test():
     return "test"
     
-if __name__ == "__main__":
-    app.run() 
+# if __name__ == "__main__":
+#     app.run()
